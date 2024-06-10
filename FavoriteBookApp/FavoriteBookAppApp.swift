ff23ceef53c2b67fb17bf433720bb5690aae6ea2
oguzhantugrulakcay <oguzhantugrulakcay@hotmail.com>
2024-06-10 19:16:50 +0300

    Initial Commit

---
+++ FavoriteBookApp/FavoriteBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookAppApp.swift
+//  FavoriteBookApp
+//
+//  Created by Oğuzhantuğrul Akçay on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
